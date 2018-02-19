77d6f2dee29d662b1a2c4221673f73274e72527e
Daniel Olson <emaildano@gmail.com>
2018-02-19 14:56:52 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './styles/scss/App.css';
+import './include/bootstrap'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
